0ff4f49a077c196bd6fad56ddd626273b2190b1a
Luiz Miguel Toller Marconatto <141162499+MiguelToller@users.noreply.github.com>
2025-08-29 14:55:26 -0300

    Initial commit: project setup with GET/POST methods

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebAPI.Model;
+using WebAPI.ViewModel;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+        }
+
+        [HttpPost]
+        public IActionResult Add(EmployeeViewModel employeeView)
+        {
+            var employee = new Employee(employeeView.Name, employeeView.Age, null);
+
+            _employeeRepository.Add(employee);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employee = _employeeRepository.Get();
+
+            return Ok(employee);
+        }
+    }
+}
